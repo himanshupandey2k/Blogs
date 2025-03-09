c6df2a3579186679c34550a6e7fa20c3bbcb1901
himanshupandey2k <himanshupandey2k@gmail.com>
2025-03-09 17:43:16 +0530

    templates

--- Blogs.py
+++ Blogs.py
@@ -1,12 +1,38 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    { 
+        'UserName' : 'Himanshu',
+        'title' : 'post1',
+        'content' : 'Job Search',
+        'date' : '15-12-2000'
+    },
+    {
+        'UserName' : 'Harshit',
+        'title' : 'post2',
+        'content' : 'Searching for love',
+        'date' : '24-3-2000'
+    },
+    {
+        'UserName' : 'Gourav',
+        'title' : 'post3',
+        'content' : 'How to be less busy',
+        'date' : '20-10-1999'
+    },
+    {
+        'UserName' : 'Dhaval',
+        'title' : 'post4',
+        'content' : 'How to prevent stalker',
+        'date' : '24-4-2000'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def homePage():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title = "About")
