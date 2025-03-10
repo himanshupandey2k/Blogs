bed99523eb378769b12eb307ac88179bd51dca5c
himanshupandey2k <himanshupandey2k@gmail.com>
2025-03-10 12:51:27 +0530

    registeration

--- Blogs.py
+++ Blogs.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'eef620ea683b020344456048dfa82a'
 
 posts = [
     { 
@@ -36,3 +39,16 @@ def homePage():
 @app.route("/about")
 def about():
     return render_template('about.html', title = "About")
+
+@app.route("/register", methods = ['GET' , 'POST'] )
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit() :
+        flash(f"Account Created for {form.userName.data}!" , "success")
+        return redirect(url_for('homePage')) 
+    return render_template('register.html', title = "Register", form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = "Login", form = form)
---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm) :
+    userName = StringField('UserName', validators=[DataRequired(), Length(min=4, max=9)])
+    email = StringField('Email', validators=[DataRequired(), Length(min=5, max=15), Email()])
+    password =  PasswordField('Password', validators=[DataRequired(), Length(min=5, max=9)])
+    confirm_Password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('SignUp')
+
+class LoginForm(FlaskForm) :
+    email = StringField('Email', validators=[DataRequired(), Length(min=5, max=15), Email()])
+    password =  PasswordField('Password', validators=[DataRequired(), Length(min=5, max=9)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
